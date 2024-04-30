0831d88cca1ae3bf847e8edbdbe616ad74808822
hariwr <hariwr25@gmail.com>
2024-04-30 07:54:36 +0700

    First Commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video STAND HERE ALONE");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio STAND HERE ALONE");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar STAND HERE ALONE");
+});
